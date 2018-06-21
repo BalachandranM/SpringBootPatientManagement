e5c310b46175b792119b40675166f5130c1c5729
bala <mnskbal@gmail.com>
2018-06-21 22:44:55 +0530

    Pushing to GIT

--- PatientManagement/src/main/java/com/PatientManagementApplication.java
+++ PatientManagement/src/main/java/com/PatientManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+//main method
 @SpringBootApplication
 @ComponentScan(value= {"com","com.beans","com.controller","com.dao","com.exceptions","com.interfaces","com.services"})
 @EnableTransactionManagement
